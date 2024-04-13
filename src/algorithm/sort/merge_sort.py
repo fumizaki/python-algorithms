0ca71e19e387d9e61fd0ab71ddf11ea37391f552
fumizaki <0238.m.0917@gmail.com>
2024-04-13 22:52:00 +0900

    fix

--- src/algorithm/sort/merge_sort.py
+++ src/algorithm/sort/merge_sort.py
@@ -1,6 +1,27 @@
 
 
 def merge_sort(array: list) -> list:
+
+    def _merge(left: list, right: list) -> list:
+        merged = []
+        left_i = 0
+        right_i = 0
+
+        while left_i < len(left) and right_i < len(right):
+            if left[left_i] <= right[right_i]:
+                merged.append(left[left_i])
+                left_i += 1
+            else:
+                merged.append(right[right_i])
+                right_i += 1
+
+        if left_i < len(left):
+            merged.extend(left[left_i:])
+        if right_i < len(right):
+            merged.extend(right[right_i:])
+
+        return merged
+
     if len(array) <= 1:
         return array
     
@@ -11,25 +32,6 @@ def merge_sort(array: list) -> list:
     left = merge_sort(left)
     right = merge_sort(right)
 
-    return merge(left, right)
+    return _merge(left, right)
 
     
-def merge(left: list, right: list) -> list:
-    merged = []
-    left_i = 0
-    right_i = 0
-
-    while left_i < len(left) and right_i < len(right):
-        if left[left_i] <= right[right_i]:
-            merged.append(left[left_i])
-            left_i += 1
-        else:
-            merged.append(right[right_i])
-            right_i += 1
-
-    if left_i < len(left):
-        merged.extend(left[left_i:])
-    if right_i < len(right):
-        merged.extend(right[right_i:])
-
-    return merged
