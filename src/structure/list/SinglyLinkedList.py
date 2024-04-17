67d77fadab7f55ec94a063e9c1d319a73f9d565e
fumizaki <0238.m.0917@gmail.com>
2024-04-17 19:46:39 +0900

    fix

--- src/structure/list/SinglyLinkedList.py
+++ src/structure/list/SinglyLinkedList.py
@@ -1,26 +1,22 @@
-from typing import Any
+from typing import Any, Optional
 
 class Node:
   
     def __init__(self, data: Any):
         self.data = data
-        self.next: Node = None
+        self.next: Optional[Node] = None
 
 
 class SinglyLinkedList:
   
     def __init__(self):
-        self.head: Node = None
-
-
-    def is_empty(self) -> bool:
-        return self.head is None
+        self.head: Optional[Node] = None
 
 
     def length(self) -> int:
         length = 0
         current_node = self.head
-        while current_node is not None:
+        while current_node:
             current_node = current_node.next
             length += 1
         return length
@@ -29,27 +25,64 @@ class SinglyLinkedList:
     def append(self, data: Any) -> None:
         new_node = Node(data)
         
-        if self.is_empty():
+        if self.head is None:
             self.head = new_node
             return
         
         current_node: Node = self.head
-        while current_node.next is not None:
+        while current_node.next:
             current_node = current_node.next
         current_node.next = new_node
 
 
-    def reverse(self) -> None:
-        if self.is_empty():
+    def remove(self, data: Any) -> None:
+        current_node = self.head
+        if current_node and current_node.data == data:
+            self.head = current_node.next
+            current_node = None
             return
         
-        prev_node = None
+        prev_node: Optional[Node] = None
+        while current_node and current_node.data != data:
+            prev_node = current_node
+            current_node = current_node.next
+
+        if prev_node is None:
+            return
+
+        if current_node is None:
+            return
+        
+        prev_node.next = current_node.next
+        current_node = None
+    
+
+    def reverse(self) -> None:
+        
+        prev_node: Optional[Node] = None
         current = self.head
-        while current is not None:
+
+        while current:
             next_temp = current.next
             current.next = prev_node
             prev_node = current
             current = next_temp
+
         self.head = prev_node
 
 
+    def reverse_recursive(self) -> None:
+
+        def _reverse(node: Optional[Node], reversed_head: Optional[Node]) -> Optional[Node]:
+            
+            if node is None:
+                return reversed_head
+
+            next_temp = node.next
+            node.next = reversed_head
+            reversed_head = node
+            node = next_temp
+            return _reverse(node, reversed_head)
+
+        reversed_head: Optional[Node] = None
+        self.head = _reverse(self.head, reversed_head)
--- tests/unit/structure/list/test_singly_linked_list.py
+++ tests/unit/structure/list/test_singly_linked_list.py
@@ -2,21 +2,11 @@ import pytest
 from src.structure.list.SinglyLinkedList import SinglyLinkedList
 
 
-def test_is_empty_with_empty_list():
-    linked_list = SinglyLinkedList()
-    is_empty = linked_list.is_empty()
-    assert is_empty == True
-
-def test_is_empty_with_non_empty_list():
-    linked_list = SinglyLinkedList()
-    linked_list.append(10)
-    is_empty = linked_list.is_empty()
-    assert is_empty == False
-
 def test_length_with_empty_list():
     linked_list = SinglyLinkedList()
     length = linked_list.length()
     assert length == 0
+
 
 def test_length_with_non_empty_list():
     linked_list = SinglyLinkedList()
@@ -25,11 +15,13 @@ def test_length_with_non_empty_list():
     length = linked_list.length()
     assert length == 2
 
+
 def test_append_with_empty_list():
     linked_list = SinglyLinkedList()
     linked_list.append(10)
     assert linked_list.head.data == 10
     assert linked_list.head.next is None
+
 
 def test_append_with_non_empty_list():
     linked_list = SinglyLinkedList()
@@ -40,10 +32,12 @@ def test_append_with_non_empty_list():
     assert current_node.next.data == 20
     assert linked_list.head.next.next is None
 
+
 def test_reverse_with_empty_list():
     linked_list = SinglyLinkedList()
     linked_list.reverse()
     assert linked_list.head is None
+
 
 def test_reverse_with_single_element_list():
     linked_list = SinglyLinkedList()
@@ -51,6 +45,7 @@ def test_reverse_with_single_element_list():
     linked_list.reverse()
     assert linked_list.head.data == 10
     assert linked_list.head.next is None
+
 
 def test_reverse_with_multiple_elements_list():
     linked_list = SinglyLinkedList()
@@ -63,3 +58,84 @@ def test_reverse_with_multiple_elements_list():
     assert current_node.next.data == 20
     assert current_node.next.next.data == 10
     assert linked_list.head.next.next.next is None
+
+
+def test_reverse_recursive_with_empty_list():
+    linked_list = SinglyLinkedList()
+    linked_list.reverse_recursive()
+    assert linked_list.head is None
+
+
+def test_reverse_recursive_with_single_element_list():
+    linked_list = SinglyLinkedList()
+    linked_list.append(10)
+    linked_list.reverse_recursive()
+    assert linked_list.head.data == 10
+    assert linked_list.head.next is None
+
+
+def test_reverse_recursive_with_multiple_elements_list():
+    linked_list = SinglyLinkedList()
+    linked_list.append(10)
+    linked_list.append(20)
+    linked_list.append(30)
+    linked_list.reverse_recursive()
+    current_node = linked_list.head
+    assert current_node.data == 30
+    assert current_node.next.data == 20
+    assert current_node.next.next.data == 10
+    assert linked_list.head.next.next.next is None
+
+
+def test_remove_from_empty_list():
+    linked_list = SinglyLinkedList()
+    linked_list.remove(10)
+    assert linked_list.head is None
+
+
+def test_remove_first_element_from_non_empty_list():
+    linked_list = SinglyLinkedList()
+    linked_list.append(10)
+    linked_list.append(20)
+    linked_list.remove(10)
+    assert linked_list.head.data == 20
+    assert linked_list.head.next is None
+
+
+def test_remove_middle_element_from_non_empty_list():
+    linked_list = SinglyLinkedList()
+    linked_list.append(10)
+    linked_list.append(20)
+    linked_list.append(30)
+    linked_list.remove(20)
+    current_node = linked_list.head
+    assert current_node.data == 10
+    assert current_node.next.data == 30
+    assert current_node.next.next is None
+
+
+def test_remove_last_element_from_non_empty_list():
+    linked_list = SinglyLinkedList()
+    linked_list.append(10)
+    linked_list.append(20)
+    linked_list.append(30)
+    linked_list.remove(30)
+    current_node = linked_list.head
+    assert current_node.data == 10
+    assert current_node.next.data == 20
+    assert current_node.next.next is None
+
+
+def test_remove_non_existent_element():
+    linked_list = SinglyLinkedList()
+    linked_list.append(10)
+    linked_list.append(20)
+    linked_list.remove(30)
+    current_node = linked_list.head
+    assert current_node.data == 10
+    assert current_node.next.data == 20
+    assert current_node.next.next is None
+
+
+
+
